bcc2eb395f0b44025fc679a2ccfa46676e8aafcb
belinea7 <zurfluh_beni@hotmail.com>
2016-12-20 19:29:56 +0100

    Update aufgabe6-3-1.php

--- aufgabe6-3-1.php
+++ aufgabe6-3-1.php
@@ -11,7 +11,7 @@
   <table id="myTable">
    <thead id="tableHead">
     <tr>
-     <th>Vorname</th>
+     <th>BENI???</th>
      <th>Nachname</th>
      <th>Geschlecht</th>
      <th>Abteilung</th>
